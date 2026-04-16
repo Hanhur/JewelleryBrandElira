b6ea75045fe76525471409fde33d2b2cd7fca3fc
Vladimir Hanhur <aqgz@email.cz>
2026-04-16 18:17:44 +0200

    Updates

--- signPage.f96fac7d.js
+++ signPage.f96fac7d.js
@@ -117,62 +117,56 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
-var bundleURL = null;
-function getBundleURLCached() {
-  if (!bundleURL) {
-    bundleURL = getBundleURL();
-  }
-  return bundleURL;
-}
-function getBundleURL() {
-  // Attempt to find the URL of the current script and use that as the base URL
-  try {
-    throw new Error();
-  } catch (err) {
-    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
-    if (matches) {
-      return getBaseURL(matches[0]);
-    }
-  }
-  return '/';
-}
-function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)?\/[^/]+(?:\?.*)?$/, '$1') + '/';
-}
-exports.getBundleURL = getBundleURLCached;
-exports.getBaseURL = getBaseURL;
-},{}],"node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
-var bundle = require('./bundle-url');
-function updateLink(link) {
-  var newLink = link.cloneNode();
-  newLink.onload = function () {
-    link.remove();
-  };
-  newLink.href = link.href.split('?')[0] + '?' + Date.now();
-  link.parentNode.insertBefore(newLink, link.nextSibling);
-}
-var cssTimeout = null;
-function reloadCSS() {
-  if (cssTimeout) {
-    return;
-  }
-  cssTimeout = setTimeout(function () {
-    var links = document.querySelectorAll('link[rel="stylesheet"]');
-    for (var i = 0; i < links.length; i++) {
-      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
-        updateLink(links[i]);
-      }
-    }
-    cssTimeout = null;
-  }, 50);
-}
-module.exports = reloadCSS;
-},{"./bundle-url":"node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"src/css/reset.css":[function(require,module,exports) {
+})({"src/images/forbes.png":[function(require,module,exports) {
+module.exports = "/forbes.4e581cc4.png";
+},{}],"src/images/elle.png":[function(require,module,exports) {
+module.exports = "/elle.9b9ca02e.png";
+},{}],"src/images/vogue.png":[function(require,module,exports) {
+module.exports = "/vogue.e247b380.png";
+},{}],"src/images/pinterest.png":[function(require,module,exports) {
+module.exports = "/pinterest.49598fe4.png";
+},{}],"src/images/instagram.png":[function(require,module,exports) {
+module.exports = "/instagram.5727c175.png";
+},{}],"src/images/facebook.png":[function(require,module,exports) {
+module.exports = "/facebook.72f9ce01.png";
+},{}],"src/images/twitter.png":[function(require,module,exports) {
+module.exports = "/twitter.8ca883f5.png";
+},{}],"src/images/chevron.svg":[function(require,module,exports) {
+module.exports = "/chevron.450016b9.svg";
+},{}],"src/pages/signPage/signPage.css":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"src/pages/signPage/signPage.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.getSignPage = getSignPage;
+var _forbes = _interopRequireDefault(require("../../images/forbes.png"));
+var _elle = _interopRequireDefault(require("../../images/elle.png"));
+var _vogue = _interopRequireDefault(require("../../images/vogue.png"));
+var _pinterest = _interopRequireDefault(require("../../images/pinterest.png"));
+var _instagram = _interopRequireDefault(require("../../images/instagram.png"));
+var _facebook = _interopRequireDefault(require("../../images/facebook.png"));
+var _twitter = _interopRequireDefault(require("../../images/twitter.png"));
+var _chevron = _interopRequireDefault(require("../../images/chevron.svg"));
+require("./signPage.css");
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
+function getSignPage() {
+  var section = document.createElement("section");
+  section.className = "section-sign";
+  var container = document.createElement("div");
+  container.className = "container";
+  var signBlock = document.createElement("div");
+  signBlock.className = "sign-block";
+  signBlock.innerHTML = "\n        <div class=\"header-content\"></div>\n        <div class=\"footer-box\">\n            <p class=\"footer-text\">\n                \u201CThe brand justifies its name. Freedom with elegance, \n                something that every woman deserves.\u201D\n            </p>\n            <img class=\"footer-img\" src=\"".concat(_forbes.default, "\" alt=\"\">\n            <img class=\"footer-img\" src=\"").concat(_elle.default, "\" alt=\"\">\n            <img class=\"footer-img\" src=\"").concat(_vogue.default, "\" alt=\"\">\n        </div>\n        <div class=\"footer-block\">\n            <h2 class=\"footer-title\">E L I R A</h2>\n            <div class=\"block-content\">\n                <div class=\"content_box\">\n                    <h3 class=\"content_title\">Support</h3>\n                    <ul class=\"content_list\">\n                        <li class=\"content_item\">\n                            <a href=\"#\" class=\"content_link\">FAQ</a>\n                        </li>\n                        <li class=\"content_item\">\n                            <a href=\"#\" class=\"content_link\">Virtual shopping</a>\n                        </li>\n                        <li class=\"content_item\">\n                            <a href=\"#\" class=\"content_link\">Shipping and Returns</a>\n                        </li>\n                        <li class=\"content_item\">\n                            <a href=\"#\" class=\"content_link\">Ring sizer</a>\n                        </li>\n                        <li class=\"content_item\">\n                            <a href=\"#\" class=\"content_link\">Contact Us</a>\n                        </li>\n                    </ul>\n                </div>\n                <div class=\"content_box\">\n                    <h3 class=\"content_title\">Company</h3>\n                    <ul class=\"content_list\">\n                        <li class=\"content_item\">\n                            <a href=\"/about\" class=\"content_link\">About Us</a>\n                        </li>\n                        <li class=\"content_item\">\n                            <a href=\"/stores\" class=\"content_link\">Stores</a>\n                        </li>\n                        <li class=\"content_item\">\n                            <a href=\"#\" class=\"content_link\">What\u2019s new?</a>\n                        </li>\n                    </ul>\n                </div>\n                <div class=\"content_box\">\n                    <h3 class=\"content_title title\">Get notified upon new arrivals</h3>\n                    <input class=\"content-input\" type=\"text\" placeholder=\"Your Email-Id Here\">\n                </div>\n                <div class=\"content_box\">\n                    <ul class=\"content_list\">\n                        <li class=\"content_item\">\n                            <a href=\"#\" class=\"content_link\">\n                                <img class=\"content-img\" src=\"").concat(_pinterest.default, "\" alt=\"\">\n                            </a>\n                        </li>\n                        <li class=\"content_item\">\n                            <a href=\"#\" class=\"content_link\">\n                                <img class=\"content-img\" src=\"").concat(_instagram.default, "\" alt=\"\">\n                            </a>\n                        </li>\n                        <li class=\"content_item\">\n                            <a href=\"#\" class=\"content_link\">\n                                <img class=\"content-img\" src=\"").concat(_facebook.default, "\" alt=\"\">\n                            </a>\n                        </li>\n                        <li class=\"content_item\">\n                            <a href=\"#\" class=\"content_link\">\n                                <img class=\"content-img\" src=\"").concat(_twitter.default, "\" alt=\"\">\n                            </a>\n                        </li>\n                    </ul>\n                </div>\n            </div>\n        </div>\n        <div class=\"conditions\">\n            <p class=\"conditions-text\">Terms and conditions</p>\n            <p class=\"conditions-text\">Privacy Policiy</p>\n            <p class=\"conditions-text\">@2021 Elira Inc</p>\n            <div class=\"conditions-box\">\n                <p class=\"conditions-text\">Lang</p>\n                <button class=\"conditions-btn\" type=\"button\">\n                    USA\n                    <img class=\"conditions-img\" src=\"").concat(_chevron.default, "\" alt=\"\">\n                </button>\n            </div>\n        </div>\n    ");
+  section.append(container);
+  container.append(signBlock);
+  return section;
+}
+},{"../../images/forbes.png":"src/images/forbes.png","../../images/elle.png":"src/images/elle.png","../../images/vogue.png":"src/images/vogue.png","../../images/pinterest.png":"src/images/pinterest.png","../../images/instagram.png":"src/images/instagram.png","../../images/facebook.png":"src/images/facebook.png","../../images/twitter.png":"src/images/twitter.png","../../images/chevron.svg":"src/images/chevron.svg","./signPage.css":"src/pages/signPage/signPage.css"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -342,4 +336,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/reset.b119aee4.js.map
+//# sourceMappingURL=/signPage.f96fac7d.js.map
